6133beb9ba8205eeed8943544a0ece3d8cdec879
Enrique Ortiz <hi@enriqueortiz.dev>
2021-03-01 17:11:25 -0400

    auth: implement reset / email validation

--- packages/backend/src/controllers/UserController.js
+++ packages/backend/src/controllers/UserController.js
@@ -1,13 +1,12 @@
 import express from "express";
 import asyncMiddleware from "middlewares/async";
-import { authenticate } from "middlewares/passport-auth";
-import EmailService from "services/EmailService";
 import Token, { TOKEN_TYPES } from "models/Token";
 import User from "models/User";
 import HttpError from "errors/http-error";
 import passport from "lib/passport-local";
 
 const DEFAULT_PROVIDER = "EMAIL";
+const TEN_DAYS = 10 * 24 * 60 * 60 * 1000;
 
 const router = express.Router();
 
@@ -16,7 +15,7 @@ function createCookieFromToken(user, statusCode, req, res) {
 
   const cookieOptions = {
     // Expires in 10 days
-    expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
+    expires: new Date(Date.now() + TEN_DAYS),
     httpOnly: true,
     secure: req.secure || req.headers["x-forwarded-proto"] === "https",
   };
@@ -84,20 +83,6 @@ router.post(
 );
 
 /**
- * Check if user is validated.
- */
-router.post(
-  "/is-validated",
-  asyncMiddleware(async (req, res) => {
-    const { email } = req.body;
-
-    const { verified = false } = await User.findOne({ email });
-
-    res.status(200).send({ verified });
-  })
-);
-
-/**
  * Validate captcha token
  */
 router.post(
@@ -111,44 +96,172 @@ router.post(
   })
 );
 
+/**
+ * Send signup email
+ */
 router.post(
   "/send-signup-email",
   asyncMiddleware(async (req, res) => {
     const { email, validationRoute } = req.body;
 
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw HttpError.BAD_REQUEST({ message: "User does not exist" });
+    }
+
+    // TODO: First check if there's an active token, and delete it
+    const staleToken = await Token.findOne({ userId: user._id });
+
+    if (staleToken) {
+      await staleToken.deleteOne();
+    }
     // TODO: Make validation token
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    const hashedResetToken = await bcrypt.hash(resetToken, SALT_ROUNDS);
+
+    const userResetToken = new Token({
+      userId: user._id,
+      token: hashedResetToken,
+      type: TOKEN_TYPES.verification,
+      createdAt: Date.now(),
+    });
+
+    await userResetToken.save();
+
+    const validationLink = `${validationRoute}/?token=${resetToken}&email=${email}`;
+    // TODO: Send email with link
+
+    return res.status(204).send();
   })
 );
 
-router.use(authenticate);
-
-/**
- * Unsubscribe email
- */
 router.post(
-  "/unsubscribe",
+  "/send-reset-email",
   asyncMiddleware(async (req, res) => {
-    /** @type {{email:string}} */
-    const data = req.body;
+    const { email, resetRoute } = req.body;
 
-    const unsubscribed = await EmailService.to(data.email).unsubscribeEmail();
+    const user = await User.findOne({ email });
 
-    res.send(unsubscribed);
+    if (!user) {
+      throw HttpError.BAD_REQUEST({ message: "User does not exist" });
+    }
+
+    // TODO: First check if there's an active token, and delete it
+    const staleToken = await Token.findOne({ userId: user._id });
+
+    if (staleToken) {
+      await staleToken.deleteOne();
+    }
+    // TODO: Make validation token
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    const hashedResetToken = await bcrypt.hash(resetToken, SALT_ROUNDS);
+
+    const userResetToken = new Token({
+      userId: user._id,
+      token: hashedResetToken,
+      type: TOKEN_TYPES.reset,
+      createdAt: Date.now(),
+    });
+
+    await userResetToken.save();
+
+    const resetLink = `${resetRoute}/?token=${resetToken}&email=${email}`;
+    // TODO: Send email with link
+
+    return res.status(204).send();
   })
 );
 
-/**
- * Subscribe email
- */
 router.post(
-  "/subscribe",
+  "/reset-password",
   asyncMiddleware(async (req, res) => {
-    /** @type {{email:string}} */
-    const data = req.body;
+    const { plainToken, password1, password2, email } = req.body;
 
-    const subscribed = await EmailService.to(data.email).subscribeEmail();
+    if (!plainToken || !password1 || !password2 || !email) {
+      throw HttpError.BAD_REQUEST({
+        message: "Missing required fields in body",
+      });
+    }
 
-    res.send(subscribed);
+    const isPasswordValid = await User.validatePassword(password1);
+
+    if (!isPasswordValid) {
+      throw HttpError.BAD_REQUEST({ message: "Password is not secure enough" });
+    }
+
+    if (password1 !== password2) {
+      throw HttpError.BAD_REQUEST({ message: "Passwords don't match" });
+    }
+
+    const storedToken = await Token.findOne({
+      email,
+      type: TOKEN_TYPES.TOKEN_RESET,
+    });
+
+    if (!storedToken) {
+      throw HttpError.BAD_REQUEST({ message: "Token is not valid" });
+    }
+
+    const isTokenMatching = await bcrypt.compare(plainToken, storedToken);
+
+    if (!isTokenMatching) {
+      throw HttpError.BAD_REQUEST({ message: "Token is not matching" });
+    }
+
+    const newHashedPassword = await bcrypt.hash(password1, SALT_ROUNDS);
+
+    await User.updateOne(
+      {
+        email,
+      },
+      { $set: { password: newHashedPassword } },
+      { new: true }
+    );
+
+    await storedToken.deleteOne();
+
+    res.status(204).send();
+  })
+);
+
+router.post(
+  "/validate-user",
+  asyncMiddleware(async (req, res) => {
+    const { plainToken, email } = req.body;
+
+    if (!plainToken || !password || !email) {
+      throw HttpError.BAD_REQUEST({
+        message: "Missing required fields in body",
+      });
+    }
+
+    const storedToken = await Token.findOne({
+      email,
+      type: TOKEN_TYPES.TOKEN_VERIFICATION,
+    });
+
+    if (!storedToken) {
+      throw HttpError.BAD_REQUEST({ message: "Token is not valid" });
+    }
+
+    const isTokenMatching = await bcrypt.compare(plainToken, storedToken);
+
+    if (!isTokenMatching) {
+      throw HttpError.BAD_REQUEST({ message: "Token is not matching" });
+    }
+
+    await User.updateOne(
+      {
+        email,
+      },
+      { $set: { verified: true } },
+      { new: true }
+    );
+
+    await storedToken.deleteOne();
+
+    res.status(204).send();
   })
 );
 
