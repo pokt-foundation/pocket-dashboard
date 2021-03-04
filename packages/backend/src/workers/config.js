25a443a2f1aab5f1beaf76e61844c5a04a22c9d0
Enrique Ortiz <hi@enriqueortiz.dev>
2021-03-04 11:54:42 -0400

    workers: add network stats worker logic

--- packages/backend/src/workers/config.js
+++ packages/backend/src/workers/config.js
@@ -3,12 +3,7 @@ import {
   stakeAppPool,
   unstakeAvailableApps,
 } from "workers/application";
-import {
-  getTotalNodeCount,
-  getTotalAppCount,
-  getTotalPoktStaked,
-  getNodeCountForChains,
-} from "workers/network";
+import { getNetworkStatsCount, getNodeCountForChains } from "workers/network";
 import {
   ONE_MINUTES,
   FIVE_MINUTES,
@@ -44,21 +39,9 @@ export const WORKERS = [
     recurrence: FIFTEEN_MINUTES,
   },
   {
-    name: "Global node counter",
+    name: "Network stats counter",
     color: "yellow",
-    workerFn: getTotalNodeCount,
-    recurrence: SIXTY_MINUTES,
-  },
-  {
-    name: "Global app counter",
-    color: "yellow",
-    workerFn: getTotalAppCount,
-    recurrence: SIXTY_MINUTES,
-  },
-  {
-    name: "Global pokt staked counter",
-    color: "yellow",
-    workerFn: getTotalPoktStaked,
+    workerFn: getNetworkStatsCount,
     recurrence: SIXTY_MINUTES,
   },
   {
--- packages/backend/src/workers/network.js
+++ packages/backend/src/workers/network.js
@@ -1,9 +1,252 @@
-async function getNodeCount(chain) {}
+/* global BigInt */
+import {
+  Account,
+  Application,
+  ApplicationParams,
+  Configuration,
+  HttpRpcProvider,
+  Node,
+  NodeParams,
+  Pocket,
+  PocketRpcProvider,
+  RpcError,
+  StakingStatus,
+  Transaction,
+  typeGuard,
+  UnlockedAccount,
+  PocketAAT,
+} from "@pokt-network/pocket-js";
+import NetworkData from "models/NetworkData";
+import Blockchains from "models/Blockchains";
+import env from "environment";
 
-export async function getTotalNodeCount() {}
+function getPocketDispatchers() {
+  const dispatchersStr = POCKET_NETWORK_CONFIGURATION.dispatchers;
 
-export async function getTotalAppCount() {}
+  if (dispatchersStr === "") {
+    return [];
+  }
 
-export async function getTotalPoktStaked() {}
+  return dispatchersStr.split(",").map(function (dispatcherURLStr) {
+    return new URL(dispatcherURLStr);
+  });
+}
 
-export async function getNodeCountForChains() {}
+const POCKET_NETWORK_CONFIGURATION = env("pocket_network");
+
+const POCKET_CONFIGURATION = new Configuration(
+  POCKET_NETWORK_CONFIGURATION.max_dispatchers,
+  POCKET_NETWORK_CONFIGURATION.max_sessions,
+  0,
+  POCKET_NETWORK_CONFIGURATION.request_timeout,
+  undefined,
+  undefined,
+  POCKET_NETWORK_CONFIGURATION.block_time,
+  undefined,
+  undefined,
+  POCKET_NETWORK_CONFIGURATION.reject_self_signed_certificates
+);
+
+const pocketInstance = new Pocket(
+  getPocketDispatchers(),
+  undefined,
+  POCKET_CONFIGURATION
+);
+
+/**
+ * @returns {HttpRpcProvider} HTTP RPC Provider.
+ */
+function getHttpRPCProvider() {
+  const httpProviderNode = POCKET_NETWORK_CONFIGURATION.http_provider_node;
+
+  if (!httpProviderNode || httpProviderNode === "") {
+    throw new Error("Invalid HTTP Provider Node: " + httpProviderNode);
+  }
+  return new HttpRpcProvider(new URL(httpProviderNode));
+}
+
+/**
+ * @returns {HttpRpcProvider | PocketRpcProvider} RPC Provider.
+ */
+async function getRPCProvider() {
+  const providerType = POCKET_NETWORK_CONFIGURATION.provider_type;
+
+  if (providerType.toLowerCase() === "http") {
+    return getHttpRPCProvider();
+  } else if (providerType.toLowerCase() === "pocket") {
+    return await getPocketRPCProvider();
+  } else {
+    // Default to HTTP RPC Provider
+    return getHttpRPCProvider();
+  }
+}
+
+/**
+ * Get Nodes data.
+ *
+ * @param {number} status Status of the nodes to retrieve.
+ *
+ * @returns {Promise<Node[]>} The nodes data.
+ * @async
+ */
+async function getNodes(status) {
+  let page = 1;
+  let nodeList = [];
+
+  const perPage = 100;
+  const pocketRpcProvider = await getRPCProvider();
+  const nodesResponse = await pocketInstance
+    .rpc(pocketRpcProvider)
+    .query.getNodes(status, undefined, BigInt(0), undefined, page, perPage);
+
+  if (nodesResponse instanceof RpcError) {
+    return [];
+  }
+
+  const totalPages = nodesResponse.totalPages;
+
+  nodesResponse.nodes.forEach((node) => {
+    nodeList.push(node);
+  });
+
+  page++;
+
+  while (page <= totalPages) {
+    const response = await pocketInstance
+      .rpc(pocketRpcProvider)
+      .query.getNodes(status, undefined, BigInt(0), undefined, page, perPage);
+
+    // Increment page variable
+    page++;
+
+    if (response instanceof RpcError) {
+      page = totalPages;
+      return;
+    }
+
+    response.nodes.forEach((node) => {
+      nodeList.push(node);
+    });
+  }
+
+  return nodesResponse.nodes;
+}
+
+/**
+ * Get Applications data.
+ *
+ * @param {number} status Status of the apps to retrieve.
+ *
+ * @returns {Promise<Application[]>} The applications data.
+ * @async
+ */
+async function getApplications(status) {
+  let page = 1;
+  let applicationList = [];
+
+  const pocketRpcProvider = await getRPCProvider();
+  const perPage = 100;
+  const applicationsResponse = await pocketInstance
+    .rpc(pocketRpcProvider)
+    .query.getApps(status, BigInt(0), undefined, page, perPage);
+
+  // Check for RpcError
+  if (applicationsResponse instanceof RpcError) {
+    return [];
+  }
+
+  // Retrieve the total pages count
+  const totalPages = applicationsResponse.totalPages;
+
+  // Retrieve the app list
+  while (page <= totalPages) {
+    const response = await pocketInstance
+      .rpc(pocketRpcProvider)
+      .query.getApps(status, BigInt(0), undefined, page, perPage);
+
+    // Increment page variable
+    page++;
+
+    // Check for error
+    if (response instanceof RpcError) {
+      page = totalPages;
+      return;
+    }
+    // Add the result to the application list
+    response.applications.forEach((app) => {
+      applicationList.push(app);
+    });
+  }
+
+  return applicationList;
+}
+
+async function getTotalNodesStaked() {
+  const stakedNodes = (await getNodes(StakingStatus.Staked)) ?? [];
+  return stakedNodes.length;
+}
+
+async function getTotalAppsStaked() {
+  const stakedApps = (await getApplications(StakingStatus.Staked)) ?? [];
+  return stakedApps.length;
+}
+
+async function getTotalPoktStaked() {
+  const stakedNodes = await getNodes(StakingStatus.Staked);
+  const stakedApps = await getApplications(StakingStatus.Staked);
+
+  const totalNodePoktStaked = stakedNodes.reduce(
+    (prev, cur) => prev + cur.stakedTokens,
+    0n
+  );
+  const totalAppPoktStaked = stakedApps.reduce(
+    (prev, cur) => prev + cur.stakedTokens,
+    0n
+  );
+
+  return BigInt(totalNodePoktStaked + totalAppPoktStaked);
+}
+
+export async function getNetworkStatsCount() {
+  const totalNodesStaked = await getTotalNodesStaked();
+  const totalAppsStaked = await getTotalAppsStaked();
+  const totalPoktStaked = await getTotalPoktStaked();
+
+  const networkStats = new NetworkData({
+    nodesStaked: totalNodesStaked,
+    appsStaked: totalAppsStaked,
+    poktStaked: totalPoktStaked.toString(),
+    createdAt: new Date(Date.now()),
+  });
+  await networkStats.save();
+}
+
+export async function getNodeCountForChains() {
+  const chainNodeCounter = new Map();
+  const stakedNodes = await getNodes(StakingStatus.Staked);
+
+  if (!stakedNodes) {
+    throw new Error("pocketJS failed when fetching nodes");
+  }
+
+  for (const { chains } of stakedNodes) {
+    for (const chainId of chains) {
+      if (!chainNodeCounter.has(chainId)) {
+        chainNodeCounter.set(chainId, 0);
+      } else {
+        const currentCount = Number(chainNodeCounter.get(chainId));
+        chainNodeCounter.set(chainId, currentCount + 1);
+      }
+    }
+  }
+
+  chainNodeCounter.forEach(async function updateChainCount(count, id) {
+    const blockchain = await Blockchains.findById(id);
+    if (!blockchain) {
+      // TODO: Add logger through dep injection to signal a non-registered chain
+      return;
+    }
+    blockchain.nodeCount = count;
+    await blockchain.save();
+  });
+}
