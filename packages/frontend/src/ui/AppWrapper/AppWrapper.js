7caa007722fa715aa5a877be94eb533569d534b2
Enrique Ortiz <hi@enriqueortiz.dev>
2021-04-07 10:55:19 -0400

    feat(UI): add ToastHub component & useToast() hub

--- packages/frontend/src/ui/AppWrapper/AppWrapper.js
+++ packages/frontend/src/ui/AppWrapper/AppWrapper.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import { Root } from "ui/Root/Root";
 import BaseStyles from "ui/BaseStyles/BaseStyles";
 import { Theme } from "ui/theme";
+import ToastHubProvider from "ui/ToastHub/ToastHub";
+
 import "@fontsource/inter";
 import "@fontsource/source-code-pro";
 
@@ -10,7 +12,9 @@ export default function AppWrapper({ children }) {
   return (
     <Root.Provider>
       <BaseStyles />
-      <Theme theme="dark">{children}</Theme>
+      <Theme theme="dark">
+        <ToastHubProvider>{children}</ToastHubProvider>
+      </Theme>
     </Root.Provider>
   );
 }
