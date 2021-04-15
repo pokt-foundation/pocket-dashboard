3cb816516a01bd9fdc033c08526625d5077004ea
Enrique Ortiz <hi@enriqueortiz.dev>
2021-04-15 15:07:59 -0400

    fix: add proper preferences button

--- packages/frontend/src/ui/icons/components/index.js
+++ packages/frontend/src/ui/icons/components/index.js
@@ -5,6 +5,7 @@ import IconDown from "ui/icons/components/IconDown";
 import IconEllipsis from "ui/icons/components/IconEllipsis";
 import IconInfo from "ui/icons/components/IconInfo";
 import IconThickCross from "ui/icons/components/IconThickCross";
+import IconPerson from "ui/icons/components/IconPerson";
 import IconPlus from "ui/icons/components/IconPlus";
 import IconQuestion from "ui/icons/components/IconQuestion";
 import IconUp from "ui/icons/components/IconUp";
@@ -16,6 +17,7 @@ export {
   IconDown,
   IconEllipsis,
   IconInfo,
+  IconPerson,
   IconPlus,
   IconThickCross,
   IconQuestion,
