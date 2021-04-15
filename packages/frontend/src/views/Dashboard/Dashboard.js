c6c6fb7ce7c4cb6c362a7f12d1c0eeea4f048997
Enrique Ortiz <hi@enriqueortiz.dev>
2021-04-15 16:26:48 -0400

    chore: add svg logo, scroll to top on route change

--- packages/frontend/src/views/Dashboard/Dashboard.js
+++ packages/frontend/src/views/Dashboard/Dashboard.js
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import "styled-components/macro";
-import { Spacer, GU } from "ui";
+import { Spacer, useTheme, GU } from "ui";
 import NavigationBar from "views/Dashboard/NavigationBar";
 import MenuPanel from "components/MenuPanel/MenuPanel";
 import { useUserApplications } from "views/Dashboard/application-hooks";
 
 export default function DashboardView({ children }) {
+  const location = useLocation();
   const { isAppsLoading, appsData } = useUserApplications();
+  const theme = useTheme();
+
+  useEffect(() => {
+    document.body.scrollTop = 0;
+  }, [location]);
 
   return (
     <div
       css={`
+        position: relative;
         width: 100%;
-        height: 100vh;
+        min-height: 100vh;
+        height: 100%;
         display: flex;
         flex-direction: row;
-        background: #051829;
+        background: ${theme.dashboardBackground};
         color: white;
+        overflow-x: hidden;
       `}
     >
       <MenuPanel appsLoading={isAppsLoading} userApps={appsData} />
