725b808cc27489d0dc82d662cfc24806fe06e147
Enrique Ortiz <hi@enriqueortiz.dev>
2021-04-16 08:49:54 -0400

    chore: fix fallback hook calling, limit entries per page on dataview

--- packages/frontend/src/views/Dashboard/Network/NetworkInfo.js
+++ packages/frontend/src/views/Dashboard/Network/NetworkInfo.js
@@ -4,6 +4,8 @@ import "styled-components/macro";
 import { DataView, Spacer, Split, GU } from "ui";
 import Box from "components/Box/Box";
 
+const PER_PAGE = 5;
+
 export default function RelayInfo({ chains, summaryData }) {
   const { within } = useViewport();
   const compactMode = within(-1, "medium");
@@ -11,19 +13,12 @@ export default function RelayInfo({ chains, summaryData }) {
   return (
     <Split
       primary={
-        <Box
-          title="Available chains"
-          css={`
-            ${!compactMode &&
-            `
-              max-height: ${56 * GU}px;
-              overflow-y: scroll;
-            `}
-          `}
-        >
+        <Box title="Available chains">
           <DataView
             fields={["Network", "ID", "Ticker"]}
             entries={chains}
+            mode={compactMode ? "list" : "table"}
+            entriesPerPage={PER_PAGE}
             renderEntry={({ description, id, network, ticker }) => [
               <p>{description || network}</p>,
               <p>{id}</p>,
