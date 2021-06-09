ca7ebc24e00f7508ec23a5bd4380d11e9a2541da
Enrique Ortiz <hi@enriqueortiz.dev>
2021-06-09 15:29:43 -0400

    chore(workers): remove logging of sensitive information

--- packages/backend/src/workers/application.ts
+++ packages/backend/src/workers/application.ts
@@ -78,7 +78,6 @@ async function stakeApplication(
   app: IPreStakedApp,
   chain = "0002"
 ): Promise<void> {
-  console.log("Got app ", app);
   const { address, passPhrase, privateKey } = app.freeTierApplicationAccount;
   const { balance } = (await getBalance(address)) as QueryBalanceResponse;
 
