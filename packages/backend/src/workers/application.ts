68a24257d0fb5db849bc1a47f75a4a6d0868e224
Enrique Ortiz <hi@enriqueortiz.dev>
2021-07-07 15:30:29 -0400

    chore(workers): remove unused variables

--- packages/backend/src/workers/application.ts
+++ packages/backend/src/workers/application.ts
@@ -1,92 +1,14 @@
-import crypto from 'crypto'
-import {
-  PocketAAT,
-  QueryAppResponse,
-  QueryBalanceResponse,
-  RawTxRequest,
-  typeGuard,
-} from '@pokt-network/pocket-js'
+import { QueryBalanceResponse, RawTxRequest } from '@pokt-network/pocket-js'
 import Application from '../models/Application'
 import PreStakedApp, { IPreStakedApp } from '../models/PreStakedApp'
-import { chains, FREE_TIER_STAKE_AMOUNT, SLOT_STAKE_AMOUNT } from './config'
+import { chains, FREE_TIER_STAKE_AMOUNT } from './config'
 import {
   createAppStakeTx,
-  createUnlockedAccount,
-  getApp,
   getBalance,
   submitRawTransaction,
   transferFromFreeTierFund,
 } from '../lib/pocket'
 import { APPLICATION_STATUSES } from '../application-statuses'
-import env, { PocketNetworkKeys } from '../environment'
-
-const MAX_POOL_SIZE = 1607
-
-type AppWorkerParams = {
-  app: IPreStakedApp
-  chain: string
-  ctx: any
-}
-
-async function createApplicationAndFund({
-  ctx,
-}: AppWorkerParams): Promise<void> {
-  const { clientPubKey, aatVersion } = env(
-    'POCKET_NETWORK'
-  ) as PocketNetworkKeys
-
-  const passphrase = crypto.randomBytes(16).toString('hex')
-  const freeTierAccount = await createUnlockedAccount(passphrase)
-
-  const gatewayAat = await PocketAAT.from(
-    aatVersion,
-    clientPubKey,
-    freeTierAccount.publicKey.toString('hex'),
-    freeTierAccount.privateKey.toString('hex')
-  )
-
-  const newAppForPool: IPreStakedApp = new PreStakedApp({
-    status: APPLICATION_STATUSES.AWAITING_FUNDS,
-    freeTierApplicationAccount: {
-      address: freeTierAccount.addressHex,
-      publicKey: freeTierAccount.publicKey.toString('hex'),
-      // @ts-ignore
-      privateKey: Application.encryptPrivateKey(
-        freeTierAccount.privateKey.toString('hex')
-      ),
-      passPhrase: passphrase,
-    },
-    gatewayAAT: {
-      version: gatewayAat.version,
-      clientPublicKey: gatewayAat.clientPublicKey,
-      applicationPublicKey: gatewayAat.applicationPublicKey,
-      applicationSignature: gatewayAat.applicationSignature,
-    },
-    createdAt: new Date(Date.now()),
-  })
-
-  const txHash = await transferFromFreeTierFund(
-    FREE_TIER_STAKE_AMOUNT.toString(),
-    freeTierAccount.addressHex
-  )
-
-  if (!txHash) {
-    ctx.logger.warn(
-      `Funds were not sent for app ${newAppForPool.freeTierApplicationAccount.address}! This is an issue with connecting to the network with PocketJS.`
-    )
-  }
-
-  newAppForPool.status = APPLICATION_STATUSES.AWAITING_STAKING
-  newAppForPool.fundingTxHash = txHash
-
-  ctx.logger.log(
-    `fillAppPool(): created app with addr ${freeTierAccount.addressHex}`
-  )
-  ctx.logger.log(
-    `fillAppPool(): sent funds to account ${freeTierAccount.addressHex} on tx ${txHash}`
-  )
-  await newAppForPool.save()
-}
 
 async function getApplicationAndFund({
   chain,
